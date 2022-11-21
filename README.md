3275067316ed5917a5c8fa323e77bb8299cc65d8
Kelin Lyu <KelinLyuACWG@gmail.com>
2022-11-20 18:22:42 -0800

    Commit.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Lab 8 - Starter
+Completed by Deze Lyu alone.
+###### Question (1)
+I choose (1) because it is mentioned in the paragraphs above: "code getting pushed to the remote repository..."
+###### Question (2)
+No, becuase it does not involve emulating user actions from start to finish.
+###### Question (3)
+No, becuase the messaging feature has the interactions between many modules such as the keyboard module (for mobile apps), spell-check module, network module, etc.
+###### Question (4)
+Maybe. If such feature is just a function retuning true or false, then yes, because it is almost independent among everything else. However, if it needs to show a fancy alert box to the user, which interacts with the alert box module, then the answer is no.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,72 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// isPhoneNumber tests
+test('isPhoneNumber test #1 (true): 111-111-1111', () => {
+    expect(functions.isPhoneNumber('111-111-1111')).toBe(true);
+});
+test('isPhoneNumber test #2 (true): 222-222-2222', () => {
+    expect(functions.isPhoneNumber('222-222-2222')).toBe(true);
+});
+test('isPhoneNumber test #3 (false): 1111111111', () => {
+    expect(functions.isPhoneNumber('1111111111')).toBe(false);
+});
+test('isPhoneNumber test #4 (false): I am a valid phone number', () => {
+    expect(functions.isPhoneNumber('I am a valid phone number')).toBe(false);
+});
+
+// isEmail tests
+test('isEmail test #1 (true): abd@def.com', () => {
+    expect(functions.isEmail('abd@def.com')).toBe(true);
+});
+test('isEmail test #2 (true): a_b_c@def.com', () => {
+    expect(functions.isEmail('a_b_c@def.com')).toBe(true);
+});
+test('isEmail test #3 (false): abc.def.com', () => {
+    expect(functions.isEmail('abc.def.com')).toBe(false);
+});
+test('isEmail test #4 (false): I am a valid email address', () => {
+    expect(functions.isEmail('I am a valid email address')).toBe(false);
+});
+
+// isStrongPassword tests
+test('isStrongPassword test #1 (true): Password1234', () => {
+    expect(functions.isStrongPassword('Password1234')).toBe(true);
+});
+test('isStrongPassword test #2 (true): A_2_5_a_k_a', () => {
+    expect(functions.isStrongPassword('A_2_5_a_k_a')).toBe(true);
+});
+test('isStrongPassword test #3 (false): qwhjdvbqdwbvjhdwbvjhwbljqbwdjhvjdqjvjdwql', () => {
+    expect(functions.isStrongPassword('qwhjdvbqdwbvjhdwbvjhwbljqbwdjhvjdqjvjdwql')).toBe(false);
+});
+test('isStrongPassword test #4 (false): 12345678', () => {
+    expect(functions.isStrongPassword('12345678')).toBe(false);
+});
+
+// isDate tests
+test('isDate test #1 (true): 01/02/2000', () => {
+    expect(functions.isDate('01/02/2000')).toBe(true);
+});
+test('isDate test #2 (true): 11/11/1111', () => {
+    expect(functions.isDate('11/11/1111')).toBe(true);
+});
+test('isDate test #3 (false): 111/111/11111', () => {
+    expect(functions.isDate('111/111/11111')).toBe(false);
+});
+test('isDate test #4 (false): I am a valid date', () => {
+    expect(functions.isDate('I am a valid date')).toBe(false);
+});
+
+// isHexColor tests
+test('isHexColor test #1 (true): #000', () => {
+    expect(functions.isHexColor('#000')).toBe(true);
+});
+test('isHexColor test #2 (true): #FFFFFF', () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+test('isHexColor test #3 (false): Red', () => {
+    expect(functions.isHexColor('Red')).toBe(false);
+});
+test('isHexColor test #4 (false): I am a valid color', () => {
+    expect(functions.isHexColor('I am a valid color')).toBe(false);
+});
